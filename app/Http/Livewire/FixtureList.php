efdc3e7b8d1d8179dceb33a30400d10c1ebd5f84
KaungKaung0 <kaungsan206@gmail.com>
2020-12-12 00:58:38 +0630

    dd inproduction removed

--- app/Http/Livewire/FixtureList.php
+++ app/Http/Livewire/FixtureList.php
@@ -17,7 +17,6 @@ class FixtureList extends Component
         }
         if(cache('matches')){
             $this->matches = cache('matches');
-            dd($this->matches);
         }else{
             $this->matches = Fixture::where('status','SCHEDULED')
             ->orderBy('id','ASC')->take(10)->get();
